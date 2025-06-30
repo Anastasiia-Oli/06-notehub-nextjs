6518a57f85d54452cc3d86e16a288d4276d10fe3
A_Oliinyk <aolejnik916@gmail.com>
2025-06-30 22:59:31 +0200

    add components and managing ssr/csr

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetails from "./NoteDetails.client";
+
+type NoteDetailsProps = {
+  params: { id: number };
+};
+
+const NoteDetailsPage = async ({ params }: NoteDetailsProps) => {
+  const queryClient = new QueryClient();
+  const id = Number(params.id);
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return <NoteDetails dehydratedState={dehydrate(queryClient)} />;
+};
+
+export default NoteDetailsPage;
---
+++ types/note.ts
@@ -0,0 +1,8 @@
+export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
